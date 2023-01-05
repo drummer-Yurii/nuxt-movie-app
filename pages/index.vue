339cb03387f28a247f43886203130f0dbb856794
Yurii Rybachok <drummer2009@ukr.net>
2023-01-05 15:03:20 +0200

    fetch hook get movie data

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,28 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies();
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1`
+        )
+        const result = await data
+        result.data.results.forEach((movie) => {
+          this.movies.push(movie)
+        })
+        console.log(this.movies);
+    }
+  }
 }
 </script>
